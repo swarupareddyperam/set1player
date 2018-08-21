c869c1c180764c8bfcfff378608bb43d4e5d6188
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-21 10:53:47 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc=new Scanner(System.in);
+		String ch=sc.next();
+		switch(ch)
+		{
+		 	            case "I":
+				System.out.print("1");
+				break;
+				case "II":
+				System.out.print("2");
+				break;
+				case "III":
+				System.out.print("3");
+				break;
+				case "IV":
+				System.out.print("4");
+				break;
+				case "V":
+				System.out.print("5");
+				break;
+				case "VI":
+				System.out.print("6");
+				break;
+				case "VII":
+				System.out.print("7");
+				break;
+				case "VIII":
+				System.out.print("8");
+				break;
+				case "IX":
+				System.out.print("9");
+				break;
+				case "X":
+				System.out.print("10");
+				break;
+				case "XI":
+				System.out.print("11");
+				break;
+				case "XII":
+				System.out.print("12");
+				break;
+				case "XIII":
+				System.out.print("13");
+				break;
+				case "XIV":
+				System.out.print("14");
+				break;
+				case "XV":
+				System.out.print("15");
+				break;
+				case "XVI":
+				System.out.print("16");
+				break;
+				case "XVII":
+				System.out.print("17");
+				break;
+				case "XVIII":
+				System.out.print("18");
+				break;
+				case "XIX":
+				System.out.print("19");
+				break;
+				case "XX":
+				System.out.print("20");
+				break;
+				
+		}
+	}
+}
