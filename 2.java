8dd2aa7d1f739781ecba044525ee0d20242eacb4
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-21 07:30:01 +0530

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.print(fact);
+		
+	}
+}
